831acaba3cba669abac005a01c450c74d96c1fdd
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-04-27 16:41:45 +0900

    add: save store data to localstorage

--- src/service/Store/ScheduleStore.ts
+++ src/service/Store/ScheduleStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { createJSONStorage, persist } from 'zustand/middleware';
 import { Schedule } from './Schedule';
 
 type ScheduleStoreState = {
@@ -15,19 +15,27 @@ const initStore: ScheduleStoreState = {
   store: {}
 }
 
-export const useScheduleStore = create<ScheduleStoreState & ScheduleStoreAction>()(devtools((set, get) => ({
-  ...initStore,
-  setScheduleData: (schedule) => {
-    return set((state) => {
-      const date = `${schedule.year}-${schedule.month}-${schedule.date}`
-      const hasBefore = get().getScheduleData(schedule.year, schedule.month, schedule.date)
-      return {
-        ...state,
-        store: { ...state.store, [date]: hasBefore.concat(schedule) }
-      }
-    })
-  },
-  getScheduleData: (year: number, month: number, date: number) => {
-    return get().store[`${year}-${month}-${date}`] || []
-  },
-})));
+export const useScheduleStore = create<ScheduleStoreState & ScheduleStoreAction>()(
+  persist(
+    (set, get) => ({
+      ...initStore,
+      setScheduleData: (schedule) => {
+        return set((state) => {
+          const date = `${schedule.year}-${schedule.month}-${schedule.date}`
+          const hasBefore = get().getScheduleData(schedule.year, schedule.month, schedule.date)
+          return {
+            ...state,
+            store: { ...state.store, [date]: hasBefore.concat(schedule) }
+          }
+        })
+      },
+      getScheduleData: (year, month, date) => {
+        return get().store[`${year}-${month}-${date}`] || []
+      },
+    }),
+    {
+      name: 'fitness-data',
+      storage: createJSONStorage(() => localStorage)
+    }
+  )
+);
