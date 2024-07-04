7a7f16fc687b95698d31e174e31831e9316e4124
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-07-04 22:45:32 +0900

    add: providers on storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,29 @@
+import type { Preview } from "@storybook/react";
+import '../src/index.css';
+import React from 'react'
+import { NextUIProvider } from '@nextui-org/react'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: 'dark'
+    },
+  },
+  decorators: [
+    (Story) => {
+      return <React.StrictMode>
+        <NextUIProvider>
+          <Story />
+        </NextUIProvider>
+      </React.StrictMode>
+    }
+  ]
+};
+
+export default preview;
