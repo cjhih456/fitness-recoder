597348700ef1ac654267ac738c24d50b81cbe915
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-09-08 00:34:34 +0900

    fix: max stack bug

--- vitePlugin/GraphqlServer/index.ts
+++ vitePlugin/GraphqlServer/index.ts
@@ -1,5 +1,10 @@
-import { PluginOption } from 'vite';
-import configServer from './configServer'
+import { PluginOption, normalizePath } from 'vite';
+import { ApolloServer } from '@apollo/server';
+import express from 'express'
+import fs from 'fs'
+import Http2 from 'http2-express-bridge'
+import { mergeSchemas, makeExecutableSchema } from '@graphql-tools/schema'
+import { expressMiddleware } from '@apollo/server/express4';
 
 interface options {
   path: string, modulePath: string[]
@@ -9,8 +14,28 @@ export default function GraphqlServer(options: options): PluginOption {
   return {
     name: 'GraphqlServer',
     apply: 'serve',
-    configureServer(server) {
-      return configServer(server, options.path, options.modulePath)
+    async configureServer(server) {
+      // return async () => {
+      const schemas = await (Promise.all(options.modulePath.map(async (path) => {
+        const gqlFile = String(fs.readFileSync(`${path}/query.gql`))
+        const schema = makeExecutableSchema({
+          typeDefs: gqlFile,
+        })
+        return schema
+      })))
+
+      const apolloServer = new ApolloServer({
+        schema: mergeSchemas({
+          schemas: schemas
+        })
+      })
+      await apolloServer.start()
+
+      const expressServer = Http2(express)
+      // const expressServer = express()
+      expressServer.use(express.json())
+      expressServer.use(expressMiddleware(apolloServer))
+      server.middlewares.use(normalizePath(options.path), expressServer)
     }
   }
 }
