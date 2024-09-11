3710a09753b54ddf5d25418a949aa187991c7e6e
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-09-11 10:52:31 +0900

    fix: init worker first

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { AlertProvider } from './components/provider/Alert/AlertProvider.tsx'
 import { HeaderProvider } from './components/provider/Header/HeaderProvider.tsx'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { link } from './HttpLink.ts'
-
+import Worker from './worker.ts'
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
   link: link,
@@ -25,17 +25,19 @@ const apolloClient = new ApolloClient({
     enabled: true
   }
 })
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <NextUIProvider>
-      <ApolloProvider client={apolloClient}>
-        <HeaderProvider>
-          <AlertProvider>
-            <App />
-          </AlertProvider>
-        </HeaderProvider>
-      </ApolloProvider>
-    </NextUIProvider>
-  </React.StrictMode>,
-)
+Worker().then((result) => {
+  console.log(result)
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <NextUIProvider>
+        <ApolloProvider client={apolloClient}>
+          <HeaderProvider>
+            <AlertProvider>
+              <App />
+            </AlertProvider>
+          </HeaderProvider>
+        </ApolloProvider>
+      </NextUIProvider>
+    </React.StrictMode>,
+  )
+})
--- src/worker.ts
+++ src/worker.ts
@@ -1,35 +1,40 @@
-if (window) {
-  const url = new URL('/src/worker/GraphqlApi.ts', import.meta.url).href
-  const sqliteUrl = new URL('/src/worker/SqliteWorker.ts', import.meta.url).href
-  if ('serviceWorker' in navigator) {
-    (async () => {
-      /**
-       * Create Service worker & Sqlite Worker
-       */
-      const workerRegistration = await navigator.serviceWorker.register(url, { type: 'module', updateViaCache: 'imports', scope: '/' })
-      const sqliteWorker = new Worker(sqliteUrl, { type: 'module', credentials: 'same-origin', name: 'sqlite' })
+export default () => {
+  return new Promise<boolean>((resolve) => {
+    if (window) {
+      const url = new URL('/src/worker/GraphqlApi.ts', import.meta.url).href
+      const sqliteUrl = new URL('/src/worker/SqliteWorker.ts', import.meta.url).href
+      if ('serviceWorker' in navigator) {
+        (async () => {
+          /**
+           * Create Service worker & Sqlite Worker
+           */
+          const workerRegistration = await navigator.serviceWorker.register(url, { type: 'module', updateViaCache: 'imports', scope: '/' })
+          const sqliteWorker = new Worker(sqliteUrl, { type: 'module', credentials: 'same-origin', name: 'sqlite' })
 
-      /**
-       * Bridge between Service worker & Sqlite Worker
-       * used Message Channel
-       */
-      navigator.serviceWorker.addEventListener('message', async (e: MessageEvent<SqliteMessage>) => {
-        const workers = await navigator.serviceWorker.getRegistrations()
-        const worker = workers.find(v => {
-          if (v.scope !== location.origin + '/') return false
-          const w = v.active || v.installing || v.waiting
-          return w?.scriptURL === url
-        })
-        if (worker) {
-          sqliteWorker.postMessage(e.data)
-        }
-      })
+          /**
+           * Bridge between Service worker & Sqlite Worker
+           * used Message Channel
+           */
+          navigator.serviceWorker.addEventListener('message', async (e: MessageEvent<SqliteMessage>) => {
+            const workers = await navigator.serviceWorker.getRegistrations()
+            const worker = workers.find(v => {
+              if (v.scope !== location.origin + '/') return false
+              const w = v.active || v.installing || v.waiting
+              return w?.scriptURL === url
+            })
+            if (worker) {
+              sqliteWorker.postMessage(e.data)
+            }
+          })
 
-      sqliteWorker.addEventListener('message', (e: MessageEvent<SqliteResultType>) => {
-        const worker = workerRegistration.active || workerRegistration.installing || workerRegistration.waiting
-        worker?.postMessage(e.data)
-      })
-    })()
+          sqliteWorker.addEventListener('message', (e: MessageEvent<SqliteResultType>) => {
+            const worker = workerRegistration.active || workerRegistration.installing || workerRegistration.waiting
+            worker?.postMessage(e.data)
+          })
+          resolve(true)
+        })()
 
-  }
+      }
+    }
+  })
 }
