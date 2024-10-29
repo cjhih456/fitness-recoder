29bb513e7f01b90996200cd98fa684082d5cd2dc
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-10-29 16:23:45 +0900

    fix: update json on file change

--- vitePlugin/LanguagePackExport/index.ts
+++ vitePlugin/LanguagePackExport/index.ts
@@ -5,7 +5,6 @@ import xlsx from 'xlsx'
 import _ from 'lodash'
 
 interface options {
-  file?: File,
   fileName?: string
   outputPath?: string
 }
@@ -18,30 +17,43 @@ interface Row {
 }
 
 export default function LanguagePackExporter(options: options): PluginOption {
+  let fileName: string | undefined = undefined
+  const makeJsonFiles = (name: string) => {
+    const xlsxFile = xlsx.readFile(name, { type: 'file' })
+    const sheet = xlsxFile.Sheets[xlsxFile.SheetNames[0]]
+    const languages = ['ko', 'en']
+    const langObj: { [k: string]: any } = {}
+    for (const row of xlsx.utils.sheet_to_json<Row>(sheet)) {
+      languages.forEach(l => {
+        _.set(langObj, [l, row.namespace, row.code.split('.')].flat(), row[l])
+      })
+    }
+    for (const lang of Object.keys(langObj)) {
+      for (const ns of Object.keys(langObj[lang])) {
+        if (options.outputPath) {
+          fs.writeFileSync(resolve(options.outputPath, lang, `${ns}.json`), JSON.stringify(langObj[lang][ns]))
+        }
+      }
+    }
+  }
   return {
     name: 'LanguagePackExporter',
     buildStart() {
-      let xlsxFile: xlsx.WorkBook | undefined = undefined
-      if (options.fileName)
-        xlsxFile = xlsx.readFile(resolve(options.fileName), { type: 'file' })
-      else if (options.file)
-        xlsxFile = xlsx.read(options.file)
-      if (xlsxFile) {
-        const sheet = xlsxFile.Sheets[xlsxFile.SheetNames[0]]
-        const languages = ['ko', 'en']
-        const langObj: { [k: string]: any } = {}
-        for (const row of xlsx.utils.sheet_to_json<Row>(sheet)) {
-          languages.forEach(l => {
-            _.set(langObj, [l, row.namespace, row.code.split('.')].flat(), row[l])
-          })
+      if (options.fileName) {
+        fileName = resolve(options.fileName)
+      }
+      if (fileName) {
+        this.addWatchFile(fileName)
+        const watchingFiles = this.getWatchFiles()
+        if (!watchingFiles.includes(fileName)) {
+          this.addWatchFile(fileName)
         }
-        for (const lang of Object.keys(langObj)) {
-          for (const ns of Object.keys(langObj[lang])) {
-            if (options.outputPath) {
-              fs.writeFileSync(resolve(options.outputPath, lang, `${ns}.json`), JSON.stringify(langObj[lang][ns]))
-            }
-          }
-        }
+        makeJsonFiles(fileName)
+      }
+    },
+    handleHotUpdate(...ctx) {
+      if (ctx[0].file === fileName && fileName) {
+        makeJsonFiles(fileName)
       }
     }
   }
