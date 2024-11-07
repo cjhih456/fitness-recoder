199b1ab6756e79c0f797c236e11a7c82c052660c
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-11-07 13:22:20 +0900

    fix: mockData auto load

--- src/service/GqlStore/AllMockedProvider.tsx
+++ src/service/GqlStore/AllMockedProvider.tsx
@@ -1,53 +1,19 @@
-import { MockedProvider } from '@apollo/client/testing';
-import { CreateExerciseByExercisePresetMock } from './Exercise/CreateExerciseByExercisePreset';
-import { CreateExerciseByScheduleMock } from './Exercise/CreateExerciseBySchedule';
-import { DeleteExerciseByIdMock } from './Exercise/DeleteExerciseById';
-import { GetExerciseListByExercisePresetIdMock } from './Exercise/GetExerciseListByExercisePresetId';
-import { GetExerciseFinishHistoryMock } from './Exercise/GetExerciseFinishHistory';
-import { GetExerciseListByScheduleIdMock } from './Exercise/GetExerciseListByScheduleId';
-import { GetExercisePresetByIdMock } from './ExercisePreset/GetExercisePresetById';
-import { GetExercisePresetListMock } from './ExercisePreset/GetExercisePresetList';
-import { GetScheduleByDateMock } from './Schedule/GetScheduleByDate';
-import { GetScheduleByIdMock } from './Schedule/GetScheduleById';
-import { GetScheduleStateByDateMock } from './Schedule/GetScheduleStatusByDate';
-import { GetSetListByExerciseIdMock } from './Set/GetSetListByExerciseId';
-import { DeleteScheduleMock } from './Schedule/DeleteSchedule';
-import { UpdateExerciseListByExercisePresetIdMock } from './Exercise/UpdateExerciseListByExercisePresetId';
-import { UpdateExerciseListByScheduleIdMock } from './Exercise/UpdateExerciseListByScheduleId';
-import { UpdateScheduleMock } from './Schedule/UpdateSchedule';
-import { CreateScheduleMock } from './Schedule/CreateSchedule';
-import { CreateSetMock } from './Set/CreateSet';
-import { UpdateSetMock } from './Set/UpdateSet';
-import { DeleteSetMock } from './Set/DeleteSet';
-import { CreateExercisePresetMock } from './ExercisePreset/CreateExercisePreset';
+import { MockedProvider, MockedResponse } from '@apollo/client/testing';
+
+const list = import.meta.glob('./**/*.ts')
+const mocks: MockedResponse[] = []
+Object.entries(list).forEach(async ([key, value]) => {
+  if (key.includes('index')) return
+  const v = await value() as { [key: string]: any }
+  Object.keys(v).forEach(k => {
+    if (k.endsWith('Mock')) {
+      mocks.push(v[k] as MockedResponse)
+    }
+  })
+})
 
 export default function AllMockedProvider({ children }: { children: React.ReactNode }) {
-  const tempMocks = [
-    CreateExerciseByExercisePresetMock,
-    CreateExerciseByScheduleMock,
-    GetExerciseListByExercisePresetIdMock,
-    GetExerciseFinishHistoryMock,
-    GetExerciseListByScheduleIdMock,
-    UpdateExerciseListByExercisePresetIdMock,
-    UpdateExerciseListByScheduleIdMock,
-    DeleteExerciseByIdMock,
-
-    CreateExercisePresetMock,
-    GetExercisePresetByIdMock,
-    GetExercisePresetListMock,
-
-    CreateScheduleMock,
-    GetScheduleByDateMock,
-    GetScheduleByIdMock,
-    GetScheduleStateByDateMock,
-    UpdateScheduleMock,
-    DeleteScheduleMock,
-
-    CreateSetMock,
-    GetSetListByExerciseIdMock,
-    UpdateSetMock,
-    DeleteSetMock
-  ].map(v => {
+  const tempMocks = mocks.map(v => {
     v.variableMatcher = v.variableMatcher ? v.variableMatcher : () => true
     return v
   })
