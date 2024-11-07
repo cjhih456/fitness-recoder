128217916a240d3064911b361b8118bd2ff6ed9d
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-11-07 16:08:03 +0900

    fix: path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import GraphqlLoader from 'vite-plugin-graphql-loader'
 import fs from 'fs'
+import { resolve } from 'path'
 import makeManifest from './vitePlugin/Manifest/MakeManifest'
 import GraphqlServer from './vitePlugin/GraphqlServer'
 import Inspect from 'vite-plugin-inspect'
@@ -40,8 +41,8 @@ export default defineConfig(({ mode, isPreview }) => {
     plugins: [
       Inspect(),
       LanguagePackExporter({
-        fileName: './LanguagePack.xlsx',
-        outputPath: './src/i18n'
+        fileName: resolve('./LanguagePack.xlsx'),
+        outputPath: resolve('./src/i18n')
       }),
       makeManifest(mode),
       GraphqlLoader(),
