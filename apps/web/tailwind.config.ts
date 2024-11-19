d2fa0eb3aa0feaf66354a61deffb100e1d044a80
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-11-19 23:38:11 +0900

    fix: package config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,7 +4,7 @@ import { Config } from 'tailwindcss';
 export default {
   content: [
     './src/**/*.{tsx,ts,css,sass}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    '../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
