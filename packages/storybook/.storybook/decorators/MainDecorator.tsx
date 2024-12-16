5cd4854856332f155f46c856ee3f2ac1c0eb4787
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-12-16 13:47:34 +0900

    fix: storybook decorator build bug

---
+++ packages/storybook/.storybook/decorators/MainDecorator.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import i18n from '@fitness/web/src/i18n'
+import { NextUIProvider } from '@nextui-org/react'
+import { I18nextProvider } from 'react-i18next'
+import AllMockedProvider from '@fitness/web/src/service/GqlStore/AllMockedProvider'
+import RootProvider from '@fitness/web/src/components/provider/RootDomProvider'
+import useApolloCache from '@fitness/web/src/hooks/apollo/useApolloCache'
+import { StoryFn } from '@storybook/react'
+
+type MainDecoratorStory = {
+  loaders?: (() => Promise<any>)[];
+} & StoryFn<any>;
+
+const MainDecorator: MainDecoratorStory = (Story, { loaded: { apolloCache } }) => {
+  return <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <NextUIProvider>
+        <RootProvider selector="div.storybook-root-path">
+          <AllMockedProvider cache={apolloCache}>
+            <div className="dark bg-background text-default-700 storybook-root-path w-[640px]" style={{ padding: '2rem' }}>
+              <Story />
+            </div>
+          </AllMockedProvider>
+        </RootProvider>
+      </NextUIProvider>
+    </I18nextProvider>
+  </React.StrictMode>
+}
+
+export const decorator = MainDecorator
+export const loaders = [
+  async () => ({
+    apolloCache: await useApolloCache()
+  })
+]
--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -1,17 +1,9 @@
 import type { Preview } from "@storybook/react";
 import './index.css';
-import React from 'react'
-import { NextUIProvider } from '@nextui-org/react'
-import { I18nextProvider } from 'react-i18next'
-import AllMockedProvider from '@fitness/web/src/service/GqlStore/AllMockedProvider'
-import RootProvider from '@fitness/web/src/components/provider/RootDomProvider'
-import useApolloCache from '@fitness/web/src/hooks/apollo/useApolloCache'
 
 import { withRouter } from 'storybook-addon-remix-react-router'
-import i18n from '@fitness/web/src/i18n'
+import { loaders, decorator as MainDecorator } from "./decorators/MainDecorator";
 
-
-const apolloCache = await useApolloCache()
 
 const preview: Preview = {
   parameters: {
@@ -25,23 +17,10 @@ const preview: Preview = {
       theme: 'dark'
     },
   },
+  loaders: loaders,
   decorators: [
     withRouter,
-    (Story) => {
-      return <React.StrictMode>
-        <I18nextProvider i18n={i18n}>
-          <NextUIProvider>
-            <RootProvider selector="div.storybook-root-path">
-              <AllMockedProvider cache={apolloCache}>
-                <div className="dark bg-background text-default-700 storybook-root-path w-[640px]" style={{ padding: '2rem' }}>
-                  <Story />
-                </div>
-              </AllMockedProvider>
-            </RootProvider>
-          </NextUIProvider>
-        </I18nextProvider>
-      </React.StrictMode>
-    }
+    MainDecorator
   ]
 };
 
