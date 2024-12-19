783023c8568f28fa5813e7f9a7cc333209ff783d
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-12-19 13:09:11 +0900

    add: descriptions

--- apps/web/src/components/Form/hooks/useFileControl.ts
+++ apps/web/src/components/Form/hooks/useFileControl.ts
@@ -11,12 +11,23 @@ export default function useFileControl<TFieldValues extends FieldValues = FieldV
   }, [max])
   const lazyFile = useRef<File[]>([])
   const [imgPreview, setImgPreview] = useState<string[]>([])
+
+  /**
+   * submit File List to RHF.field & current file list
+   * @param v new File List
+   * @param field RHF.field object
+   */
   const updateFileValueOnForm = useCallback((v: File[], field?: ControllerRenderProps<TFieldValues>) => {
     lazyFile.current = v
     field && field.onChange(lazyFile.current)
     onSelectFile && onSelectFile(lazyFile.current)
   }, [onSelectFile])
 
+  /**
+   * remove File by index & submit into field
+   * @param index index number
+   * @param field RHF.field object
+   */
   const deleteImageFile = useCallback((index: number, field?: ControllerRenderProps<TFieldValues>) => {
     const tempArray = [...lazyFile.current]
     tempArray.splice(index, 1)
@@ -29,6 +40,11 @@ export default function useFileControl<TFieldValues extends FieldValues = FieldV
     return tempArray
   }, [setImgPreview, updateFileValueOnForm])
 
+  /**
+   * add File List & submit into field
+   * @param file File list which want to add
+   * @param field RHF.field object
+   */
   const appendImageFile = useCallback((file: (File | null)[], field?: ControllerRenderProps<TFieldValues>) => {
     const tempList = [...lazyFile.current]
     const newFileUrlList: string[] = [...imgPreview]
@@ -46,17 +62,28 @@ export default function useFileControl<TFieldValues extends FieldValues = FieldV
     updateFileValueOnForm(tempList, field)
   }, [maxNumber, imgPreview, updateFileValueOnForm])
 
+  /**
+   * remove a file into File list & submit 
+   * @param idx index of File, which want to add
+   * @param e event that will stop propagation.
+   * @param field RHF.field object
+   */
   const cancelImage = useCallback((idx?: number, e?: BaseSyntheticEvent, field?: ControllerRenderProps<TFieldValues>) => {
     e && e.stopPropagation()
     if (typeof idx === 'number')
       deleteImageFile(idx, field)
   }, [deleteImageFile])
 
+  /**
+   * add File List & submit into field
+   * @param e File list selectd event
+   * @param field RHF.field object
+   */
   const onInputFileChange = useCallback((e: ChangeEvent<HTMLInputElement>, field?: ControllerRenderProps<TFieldValues>) => {
     const file = e.target.files
     if (!file) return
     const fileList = Array(file.length).fill(0).map((_, i) => file.item(i))
     appendImageFile(fileList, field)
   }, [appendImageFile])
-  return { onInputFileChange, cancelImage, imgPreview }
+  return { onInputFileChange: onInputFileChange, cancelImage: cancelImage, imgPreview }
 }
