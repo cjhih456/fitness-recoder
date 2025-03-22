ef04aae11d9c97e3b8252c02f8e3115ef352ef4f
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-03-22 20:39:30 +0900

    remove: not use lazy query

--- apps/web/src/hooks/apollo/Schedule/useLazyGetScheduleByDate.ts
+++
@@ -1,9 +0,0 @@
-import { useLazyQuery } from '@apollo/client';
-import GetScheduleByDateGql from '@hooks/apollo/Schedule/graphql/query/GetScheduleByDateGql';
-
-export default function useLazyGetScheduleByDate() {
-  return useLazyQuery<
-    GetScheduleByDateResponse,
-    GetScheduleByDateVariable
-  >(GetScheduleByDateGql)
-}
