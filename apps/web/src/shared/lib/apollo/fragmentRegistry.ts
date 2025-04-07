7d01c356598f0368b5389969e7ac30f53e643249
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-04-07 19:09:05 +0900

    fix: apollo graphql errors

--- apps/web/src/shared/lib/apollo/fragmentRegistry.ts
+++ apps/web/src/shared/lib/apollo/fragmentRegistry.ts
@@ -1,13 +1,14 @@
 import { createFragmentRegistry } from '@apollo/client/cache';
 import { isDocumentNode } from '@apollo/client/utilities';
 
-const fragmentFiles = import.meta.glob(['/src/entities/**/*ragment.ts'], { eager: true })
+const fragmentFiles = import.meta.glob(['../../../entities/**/*ragment.ts'], { eager: true })
 const fragmentRegistry = createFragmentRegistry()
 
 for (const key in fragmentFiles) {
   const fragment = fragmentFiles[key] as Record<string, any>
   if (isDocumentNode(fragment?.default)) {
     fragmentRegistry.register(fragment.default)
+    console.log(key)
   }
 }
 
