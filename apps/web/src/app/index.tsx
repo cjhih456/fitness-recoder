4cbf8302215b5a58786e226c64a67c36a33bf111
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-04-08 00:45:57 +0900

    fix: apollo cache issue

--- apps/web/src/app/index.tsx
+++ apps/web/src/app/index.tsx
@@ -4,19 +4,24 @@ import React, { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { I18nextProvider } from 'react-i18next'
 import { BrowserRouter } from 'react-router-dom'
-import firebase from '@shared/lib/firebase/firebase.ts'
 import i18n from '@shared/config/i18n/index.ts'
-import apolloClient from '@shared/lib/apollo'
+import getApolloClient from '@shared/lib/apollo'
+import firebase from '@shared/lib/firebase/firebase.ts'
 import { baseURL } from '@shared/lib/utils/index.ts'
 import App from './App.tsx'
+import { getFragmentFiles } from './lib/getFragmentFiles.ts'
 import Worker from './worker.ts'
 
 firebase()
+
+const fragmentList = getFragmentFiles()
+const client = getApolloClient({ fragmentList })
+
 Worker().then(() => {
   createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <I18nextProvider i18n={i18n}>
-        <ApolloProvider client={apolloClient}>
+        <ApolloProvider client={client}>
           <BrowserRouter basename={baseURL('/')}>
             <Suspense fallback={<div className="w-screen h-screen flex items-center justify-center"><Spinner /></div>}>
               <App />
