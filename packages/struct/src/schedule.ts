c127ff61243e0aa27e6b2077c02ff6ad0f404602
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-04-16 16:51:55 +0900

    fix: schedule structure

--- packages/struct/src/schedule.ts
+++ packages/struct/src/schedule.ts
@@ -9,22 +9,25 @@ export const IScheduleSchema = z.object({
   month: z.number(),
   date: z.number(),
   type: IScheduleTypeSchema,
+  start: z.number(),
   beforeTime: z.number(),
   breakTime: z.number(),
   workoutTimes: z.number(),
 })
 
-export const IScheduleCreateSchema = IScheduleSchema.omit({ id: true })
+export const IScheduleCreateSchema = IScheduleSchema.pick({
+  year: true,
+  month: true,
+  date: true,
+  type: true
+})
 
 export const IScheduleWithExerciseListSchema = IScheduleSchema.extend({
   exerciseList: z.array(IExerciesSchema),
 })
 export declare namespace Schedule {
   type IType = z.infer<typeof IScheduleTypeSchema>
-
   type CreateType = z.infer<typeof IScheduleCreateSchema>
-
   type Data = z.infer<typeof IScheduleSchema>
-
   type WithExerciseList = z.infer<typeof IScheduleWithExerciseListSchema>
 }
