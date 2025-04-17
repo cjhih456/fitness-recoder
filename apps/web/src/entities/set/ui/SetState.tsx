115540e50453197107649488b4d206c847d4b498
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-04-18 00:21:40 +0900

    fix: set state display

--- apps/web/src/entities/set/ui/SetState.tsx
+++ apps/web/src/entities/set/ui/SetState.tsx
@@ -1,25 +1,31 @@
 import { MdCheck } from 'react-icons/md';
 import { useGetSetListByExerciseId } from '@entities/set/api';
-import StateRender from '@shared/ui/StateRender';
+import { EnumRender } from '@shared/ui/StateRender';
 
 interface SetStateProps {
   exerciseDataId: number
 }
+
+type SetStateType = 'done' | 'notDone' | 'notStarted'
 
 export default function SetState({
   exerciseDataId
 }: SetStateProps) {
   const { data } = useGetSetListByExerciseId(exerciseDataId)
 
+  let state: SetStateType = 'notDone'
   const doneCount = data.getSetListByExerciseId.filter(v => v.isDone).length
+  if (data.getSetListByExerciseId.length === 0) state = 'notStarted'
+  else if (doneCount === data.getSetListByExerciseId.length) state = 'done'
 
-  return <StateRender.Boolean
-    state={Boolean(data.getSetListByExerciseId.length) && doneCount === data.getSetListByExerciseId.length}
+  return <EnumRender
+    state={state}
     render={{
-      true: () => <div className="flex justify-center items-center w-[16px] h-[16px] rounded-full bg-primary text-white">
+      done: () => <div className="flex justify-center items-center w-[16px] h-[16px] rounded-full bg-primary text-white">
         <MdCheck size="0.75rem"></MdCheck>
       </div>,
-      false: () => <div> {doneCount}/{data.getSetListByExerciseId.length} </div>
+      notDone: () => <div> {doneCount}/{data.getSetListByExerciseId.length} </div>,
+      notStarted: () => <></>
     }}
   />
 }
