a994ef8df15ddda73421b8893add4cda88454e22
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-04-18 15:27:15 +0900

    fix: useHeaderHandler hook

--- apps/web/src/widgets/header/hooks/useHeaderHandler.tsx
+++ apps/web/src/widgets/header/hooks/useHeaderHandler.tsx
@@ -2,10 +2,14 @@ import { useSetAtom } from 'jotai';
 import { useEffect } from 'react'
 import { headerContentAtom } from '../lib/atom';
 
-export default function useHeaderHandler(header: string | undefined) {
+export default function useHeaderHandler(header?: string) {
   const setHeader = useSetAtom(headerContentAtom)
   useEffect(() => {
+    if (!header) return
     setHeader(header)
   }, [header, setHeader])
   useEffect(() => () => setHeader(''), [])
+  return {
+    setHeader
+  }
 }
