8eeacda01b550d5dcce2ab7b9b86d695e1190bff
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-04-19 17:44:01 +0900

    fix: add mock path

--- apps/web/src/app/lib/getMockQueryFiles.ts
+++ apps/web/src/app/lib/getMockQueryFiles.ts
@@ -2,7 +2,7 @@ import type { InMemoryCache } from '@apollo/client'
 import type { MockedResponse } from '@apollo/client/testing'
 
 export const getMockQueryFiles = (cache: InMemoryCache) => {
-  const list = import.meta.glob(['../../entities/**/*.mock.ts'], { eager: true, import: 'default' })
+  const list = import.meta.glob(['../../entities/**/*.mock.ts', '../../features/**/*.mock.ts'], { eager: true, import: 'default' })
   return Object.keys(list).map((key) => {
     const mockResponse = list[key] as MockedResponse
     mockResponse.variableMatcher = mockResponse.variableMatcher ?? (() => true)
